c0ae3fca01ea7c2a0599f2f66bbfe954d7056223
TaiChi <anothai.0978452316@gmail.com>
2024-02-24 18:00:43 +0700

    do homework 112 soy successfully 11

---
+++ University/Cos2101/Class/HW/HW1/HW1-4.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    double r; // r equal radius
+    double area;
+    cout << "radius : ";
+    cin >> r; // Enter value radius
+    area = 3.14 * r * r;
+    cout << "Result area circle : " << area << endl;
+    return 0;
+}

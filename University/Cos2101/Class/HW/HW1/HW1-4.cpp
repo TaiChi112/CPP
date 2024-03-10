e24bc85e7ad8a1d5c9c0274a75fffb11b5d2b087
TaiChi <anothai.0978452316@gmail.com>
2024-03-10 15:05:35 +0700

    modified HW1-4.cpp i forgot what i edit

--- University/Cos2101/Class/HW/HW1/HW1-4.cpp
+++ University/Cos2101/Class/HW/HW1/HW1-4.cpp
@@ -4,11 +4,10 @@ using namespace std;
 
 int main()
 {
-    double r; // r equal radius
-    double area;
+    double r, area; // r equal radius declare variable
     cout << "radius : ";
-    cin >> r; // Enter value radius
-    area = 3.14 * r * r;
+    cin >> r;            // Enter value radius
+    area = 3.14 * r * r; // formula to find area of circle
     cout << "Result area circle : " << area << endl;
     return 0;
 }

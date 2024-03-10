53cad93604d8ccfde2b16aadea4414fbe763fae1
TaiChi <anothai.0978452316@gmail.com>
2024-03-10 15:04:19 +0700

    modified HW1-3.md delete comment in this file

--- University/Cos2101/Class/HW/HW1/HW1-3.cpp
+++ University/Cos2101/Class/HW/HW1/HW1-3.cpp
@@ -4,16 +4,16 @@ int main()
 {
     int num1, num2, SUM;
     cout << "Enter number 1 : ";
-    cin >> num1; //+ Enter number 1 : 34
+    cin >> num1;
     cout << "Enter number 2 : ";
-    cin >> num2;       //+ Enter number 5 : 5
-    SUM = num1 + num2; //! 34+5= 39
+    cin >> num2;
+    SUM = num1 + num2;
     cout << num1 << "+" << num2 << "= " << SUM << endl;
-    SUM = num1 - num2; //! 34-5= 29
+    SUM = num1 - num2;
     cout << num1 << "-" << num2 << "= " << SUM << endl;
-    SUM = num1 * num2; //! 34*5= 170
+    SUM = num1 * num2;
     cout << num1 << "*" << num2 << "= " << SUM << endl;
-    SUM = num1 / num2; //! 34/5= 6
+    SUM = num1 / num2;
     cout << num1 << "/" << num2 << "= " << SUM << endl;
     return 0;
 }

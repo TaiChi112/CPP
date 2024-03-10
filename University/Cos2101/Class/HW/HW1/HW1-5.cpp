daa2dece0f1cf13b580b01bf11d275c9d16f1b96
TaiChi <anothai.0978452316@gmail.com>
2024-03-10 15:06:10 +0700

    modified HW1-5.cpp write comment explain

--- University/Cos2101/Class/HW/HW1/HW1-5.cpp
+++ University/Cos2101/Class/HW/HW1/HW1-5.cpp
@@ -5,13 +5,13 @@ using namespace std;
 int main()
 {
     double m;        // money
-    double r;        // interest rate %
+    double r;        // rate percent
     double interest; //
     double net;      // compound interest
     cout << "Enter your money : ";
-    cin >> m;
+    cin >> m;// input money
     cout << "Enter your interest rate % : ";
-    cin >> r;
+    cin >> r;// input rate
     interest = (m * r) / 100; // formula to find interest
     net = m * interest;       // formula to find net
     cout << "Interest : " << interest << endl;

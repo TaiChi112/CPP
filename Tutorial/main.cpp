ea20f5b182b4d1e3f9ee0266a82d6aae60260a92
TaiChi <anothai.0978452316@gmail.com>
2024-06-10 20:22:09 +0700

    feat : I try to write code dynamic CPP

--- Tutorial/main.cpp
+++ Tutorial/main.cpp
@@ -1,16 +1,29 @@
 #include <iostream>
-#include <io.h>
-#include <fcntl.h>
 
 using namespace std;
 
+struct Node
+{
+    int data;
+    Node *AdNextVal;
+};
+
 int main()
 {
-    wcout << "Hello World" << endl;
-    cout << "Hello World" << endl;
-    _setmode(_fileno(stdout), _O_U16TEXT);
 
-    // พิมพ์ข้อความภาษาไทยออกทางหน้าจอ
-    wcout << L"สวัสดีชาวโลก" << endl;
+    Node *ptr1 = new Node;
+    int *ptr2 = new int;
+    Node node1;
+    int node2;
+
+    Node *ptr3 = &node1;
+    int *ptr4 = &node2;
+    Node ads = *ptr1;
+    int ads2 = *ptr2;
+
+    cout << "Struct Node ads1 keeping pointer ptr1 " << ptr1 << endl;
+
+    delete ptr1, ptr2, &node1, &node2, ptr3, ptr4;
+    cout << "Delete : " << "ptr1, ptr2, &node1, &node2, ptr3, ptr4" << endl;
     return 0;
 }

df3e4865351ded2ae93625ca6b88bc0f44d19b7a
TaiChi <anothai.0978452316@gmail.com>
2024-06-19 22:24:19 +0700

    Learn : type of category  pointer

--- Tutorial/src/main.cpp
+++ Tutorial/src/main.cpp
@@ -4,25 +4,28 @@
 
 using namespace std;
 
+void printNumber1(int *numPtr)
+{
+    cout << *numPtr << endl;
+}
+void printChar1(char *charPtr)
+{
+    cout << *charPtr << endl;
+}
+void printNumber2(int numPtr)
+{
+    cout << numPtr << endl;
+}
+void printChar2(char charPtr)
+{
+    cout << charPtr << endl;
+}
+
 int main()
 {
-    int x1 = 5;
-    int x2(5);
-    cout << "value var x1 : " << x1 << endl;
-    cout << "value var x2 : " << x2 << endl;
-    cout << "address var x1 : " << &x1 << endl;
-    cout << "address var x2 : " << &x2 << endl;
-    int *y1 = &x1;
-    int *y2 = &x2;
-    cout << "value pointer y1 which keeping address of x1 : " << *y1 << endl;
-    cout << "value pointer y1 which keeping address of x1 : " << *y2 << endl;
-    cout << "address of x1 :" << y1 << endl;
-    cout << "address of x1 :" << y2 << endl;
-    cout << "address pointer y1 : " << &y1 << endl;
-    cout << "address pointer y1 : " << &y2 << endl;
-
-    delete y1;
-    delete y2;
-
+    int number = 5;
+    printNumber1(&number);
+    char character = 'a';
+    printChar2(&character);
     return 0;
 }

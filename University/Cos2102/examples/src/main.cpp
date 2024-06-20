cf7d34740fa3b5600085b1d83da011cc6301b37a
TaiChi <anothai.0978452316@gmail.com>
2024-06-21 03:16:23 +0700

    refactor : get attribute and show attribute

--- University/Cos2102/examples/src/main.cpp
+++ University/Cos2102/examples/src/main.cpp
@@ -9,11 +9,16 @@ class Jewelry
 
 public:
       Jewelry() : price(990){};
-      Jewelry(double price, int madeForm) : price(price), madeForm(madeForm){};
+      // Jewelry(double price, int madeForm) : price(price), madeForm(madeForm){};
+      Jewelry(double price, int madeForm)
+      {
+            setPrice(price);
+            setMadeForm(madeForm);
+      }
       ~Jewelry() { cout << "Destructuring class Jewelry" << endl; }
       void setPrice(double price)
       {
-            if (price = > 0)
+            if (price >= 0)
             {
                   this->price = price;
             }
@@ -33,6 +38,12 @@ public:
                   cout << "madeForm must be 1, 2 or 3" << endl;
             }
       }
+      double getPrice() { return price; }
+      int getMadeForm() { return madeForm; }
+
+      virtual void show() = 0;
+
+      void showPrice() { cout << "price: " << this->getPrice() << endl; }
 };
 class Gem
 {
@@ -52,6 +63,11 @@ public:
                   cout << "type must be 1, 2, 3, 4 or 5" << endl;
             }
       }
+      int getType() { return type; }
+
+      virtual void show() = 0;
+
+      void showType() { cout << "type: " << this->getType() << endl; }
 };
 class Ring : public Jewelry
 {
@@ -73,6 +89,28 @@ public:
                   cout << "num must > 0" << endl;
             }
       }
+      Gem **getGems() { return gems; }
+      int getNum() { return num; }
+
+      void show()
+      {
+            this->showPrice();
+            this->showType();
+            cout << "num: " << this->getNum() << endl;
+            for (int i = 0; i < this->getNum(); i++)
+            {
+                  this->getGems()[i]->show();
+            }
+      }
+
+      void showGems()
+      {
+            cout << "num: " << this->getNum() << endl;
+            for (int i = 0; i < this->getNum(); i++)
+            {
+                  this->getGems()[i]->show();
+            }
+      }
 };
 class Necklace : public Jewelry
 {
@@ -82,8 +120,21 @@ public:
       Necklace() : Jewelry(22000, 1), pendant(true){};
       ~Necklace() { cout << "Destructuring class Necklace" << endl; }
       void setPendant(bool pendant) { this->pendant = pendant; }
+
+      bool getPendant() { return pendant; }
+
+      void show()
+      {
+            this->showPrice();
+            this->showType();
+            cout << "pendant: " << this->getPendant() << endl;
+      }
+
+      void showPendant() { cout << "pendant: " << this->getPendant() << endl; }
 };
 int main()
 {
+      Jewelry(990, 1);
+      cout << "End of main" << endl;
       return 0;
 }

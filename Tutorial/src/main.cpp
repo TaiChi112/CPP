8a61d60c3167626b58db70522fd3dc925ad37fc0
TaiChi <anothai.0978452316@gmail.com>
2024-06-22 23:38:24 +0700

    refactor : DSA Linked List C++

--- Tutorial/src/main.cpp
+++ Tutorial/src/main.cpp
@@ -4,26 +4,49 @@
 
 using namespace std;
 
+struct Node
+{
+    int data;
+    Node *next;
+    Node(){};
+    Node(int data) : data(data), next(NULL){};
+};
+Node *insertNode(Node *head, int data)
+{
+    Node *newNode = new Node(data);
+    if (head == NULL)
+    {
+        head = newNode;
+    }
+    else
+    {
+        Node *temp = head;
+        while (temp->next != NULL)
+        {
+            temp = temp->next;
+        }
+        temp->next = newNode;
+    }
+    return head;
+}
+void displayNode(Node *head)
+{
+    Node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+}
 int main()
 {
-    int luckNumber[5];
-    // cout << luckNumber << endl;
-    // cout << &luckNumber[0] << endl;
-    // cout << &luckNumber[1] << endl;
-    // cout << &luckNumber[2] << endl;
-    // cout << &luckNumber[3] << endl;
-    // cout << luckNumber[3] << endl;
-    // cout << *(luckNumber + 3) << endl;
+    Node *head = new Node(1);
+    head->next = new Node(2);
 
-    for (int i = 0; i <= 4; i++)
-    {
-        cout << "Number : ";
-        cin >> luckNumber[i];
-    }
-    for (int i = 0; i <= 4; i++)
-    {
-        cout << *luckNumber + i << endl;
-    }
+    insertNode(head, 3);
+    insertNode(head, 113);
+    insertNode(head, 112);
+    displayNode(head);
 
     return 0;
 }

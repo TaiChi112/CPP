3756ebe80e99c5d86a5de20eabf648b1c36c286e
TaiChi <anothai.0978452316@gmail.com>
2024-06-26 00:38:43 +0700

    modify : normalize decalration variable

--- Tutorial/src/main.cpp
+++ Tutorial/src/main.cpp
@@ -7,23 +7,7 @@ using namespace std;
 
 int main()
 {
-    Types char1;
-    char1.setChar('A');
-    char1.showChar();
-
-    MyInt int1;
-    int1.setInt(112);
-    int1.showInt();
-
-    MyString string1;
-    string1.setString("Hi C++");
-    string1.showString();
-    // int1 = 112;
-    // cout << "Value int1 : " << int1 << endl;
-    // cout << "Address int1 : " << &int1 << endl;
-
-    // string1 = "Hello World!";
-    // cout << "Value string1 : " << string1 << endl;
-    // cout << "Value string1 : " << &string1 << endl;
+    int x1;
+    x1 = 112;
     return 0;
 }

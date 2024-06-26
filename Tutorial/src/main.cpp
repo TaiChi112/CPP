07225346d746502839b1635a361e109cb00a3c4d
TaiChi <anothai.0978452316@gmail.com>
2024-06-26 15:59:36 +0700

    create : new class object person and testing object

--- Tutorial/src/main.cpp
+++ Tutorial/src/main.cpp
@@ -1,18 +1,29 @@
 #include <iostream>
-#include "class.h"
-#include "mid.h"
-#include "type.h"
 
 using namespace std;
 
-void show(int *x)
+class Person1
 {
-    cout << "int : " << *x << endl;
+    string name;
+    int age;
+
+public:
+    Person1() {}
+    Person1(string name, int age) : name(name), age(age) {}
+    void setName(string name) { this->name = name; }
+    void setAge(int age) { this->age = age; }
+    string getName() { return name; }
+    int getAge() { return age; }
+    ~Person1() { cout << "Destructor called" << endl; }
+    void show()
+    {
+        cout << "Name: " << name << endl;
+        cout << "Age: " << age << endl;
+    }
 };
 int main()
 {
-    int x1;
-    x1 = 112;
-    show(&x1);
+    Person1 person1("John", 30);
+    person1.show();
     return 0;
-};
+}

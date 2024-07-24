5e5c3e10aaf6be1eb68af1ac083ab6e2fc4b34cc
TaiChi <anothai.0978452316@gmail.com>
2024-07-25 02:44:58 +0700

    learn : DS linked list CRUD

--- Practices/demo.cpp
+++ Practices/demo.cpp
@@ -1,32 +1,147 @@
 #include <iostream>
 
-using namespace std;
-
-struct Node1
+// Define the structure for each node in the linked list
+struct Node
 {
       int data;
-      Node1 *address;
+      Node *next;
 };
 
+// Function to create a new node
+Node *createNode(int value)
+{
+      Node *newNode = new Node();
+      newNode->data = value;
+      newNode->next = nullptr;
+      return newNode;
+}
 
-int main(){
-      Node1 node1,node2,node3;
-      node1.data = 112;
-      node1.address = &node2;
-      node1.data = 113;
-      node1.address = &node3;
-      node1.data = 114;
-      node1.address = nullptr;
-      
-      void display(Node1 *node){
-            for (int i = 0; i < 3; i++)
-            {
-                  /* code */
-            }
-            
+// Function to insert a new node at the beginning of the linked list
+void insertNode(Node **head, int value)
+{
+      Node *newNode = createNode(value);
+      newNode->next = *head;
+      *head = newNode;
+      std::cout << "Node with value " << value << " inserted successfully." << std::endl;
+}
 
+// Function to display the linked list
+void displayList(Node *head)
+{
+      if (head == nullptr)
+      {
+            std::cout << "Linked list is empty." << std::endl;
+            return;
       }
 
-           
-    return 0;
+      Node *temp = head;
+      std::cout << "Linked list: ";
+      while (temp != nullptr)
+      {
+            std::cout << temp->data << " ";
+            temp = temp->next;
+      }
+      std::cout << std::endl;
+}
+
+// Function to delete a node from the linked list
+void deleteNode(Node **head, int value)
+{
+      if (*head == nullptr)
+      {
+            std::cout << "Linked list is empty. Deletion failed." << std::endl;
+            return;
+      }
+
+      Node *temp = *head;
+      Node *prev = nullptr;
+
+      // If the node to be deleted is the head node
+      if (temp != nullptr && temp->data == value)
+      {
+            *head = temp->next;
+            delete temp;
+            std::cout << "Node with value " << value << " deleted successfully." << std::endl;
+            return;
+      }
+
+      // Find the node to be deleted
+      while (temp != nullptr && temp->data != value)
+      {
+            prev = temp;
+            temp = temp->next;
+      }
+
+      // If the node is not found
+      if (temp == nullptr)
+      {
+            std::cout << "Node with value " << value << " not found in the linked list. Deletion failed." << std::endl;
+            return;
+      }
+
+      // Unlink the node from the linked list
+      prev->next = temp->next;
+      delete temp;
+      std::cout << "Node with value " << value << " deleted successfully." << std::endl;
+}
+
+// Function to update the value of a node in the linked list
+void updateNode(Node *head, int oldValue, int newValue)
+{
+      if (head == nullptr)
+      {
+            std::cout << "Linked list is empty. Update failed." << std::endl;
+            return;
+      }
+
+      Node *temp = head;
+      bool found = false;
+
+      // Find the node to be updated
+      while (temp != nullptr)
+      {
+            if (temp->data == oldValue)
+            {
+                  temp->data = newValue;
+                  found = true;
+                  break;
+            }
+            temp = temp->next;
+      }
+
+      if (found)
+      {
+            std::cout << "Node with value " << oldValue << " updated to " << newValue << " successfully." << std::endl;
+      }
+      else
+      {
+            std::cout << "Node with value " << oldValue << " not found in the linked list. Update failed." << std::endl;
+      }
+}
+
+int main()
+{
+      Node *head = nullptr;
+
+      // Insert nodes into the linked list
+      insertNode(&head, 10);
+      insertNode(&head, 20);
+      insertNode(&head, 30);
+
+      // Display the linked list
+      displayList(head);
+
+      // Delete a node from the linked list
+      deleteNode(&head, 20);
+
+      // Display the updated linked list
+      displayList(head);
+
+      // Update a node in the linked list
+      updateNode(head, 10, 50);
+
+      // Display the updated linked list
+      displayList(head);
+
+      return 0;
 }

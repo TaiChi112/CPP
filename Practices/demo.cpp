a55ac50add1910c18d81434d247b95dc4e9db4ab
TaiChi <anothai.0978452316@gmail.com>
2024-07-25 22:52:28 +0700

    feat : fn display value in array by index & test successfully fn

--- Practices/demo.cpp
+++ Practices/demo.cpp
@@ -9,11 +9,18 @@ void gets(int array[], int size)
             cout << array[i] << " : " << &array[i] << endl;
       }
 }
-
+void getByIndex(int array[], int size, int index)
+{
+      if (index >= 0 && index < size)
+      {
+            cout << array[index] << " : " << &array[index] << endl;
+      }
+}
 int main()
 {
       int myArray[5]{112, 113, 114, 112, 118};
       int size{sizeof(myArray) / sizeof(myArray[0])};
       gets(myArray, size);
+      getByIndex(myArray, size, 2);
       return 0;
 }

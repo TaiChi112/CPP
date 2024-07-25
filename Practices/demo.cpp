bf059a258a673cc5985209fea9cf62808200bda9
TaiChi <anothai.0978452316@gmail.com>
2024-07-25 23:05:33 +0700

    handle : condition display

--- Practices/demo.cpp
+++ Practices/demo.cpp
@@ -20,8 +20,19 @@ void getByIndex(int array[], int size, int index)
             cerr << "Error : Invalid index " << index << " Valid range is 0 to " << (size - 1) << endl;
       }
 }
-void getValueStartIndexToEndIndex(int array[], int size, int startIndex,int endIndex){
-      if(startIndex >= 0 && startIndex < size && endIndex >= 0 && endIndex < size){}
+void getValueStartIndexToEndIndex(int array[], int size, int startIndex, int endIndex)
+{
+      if (startIndex >= 0 && startIndex < size && endIndex >= 0 && endIndex < size)
+      {
+            for (int i = startIndex; i <= endIndex; i++)
+            {
+                  cout << array[i] << " : " << &array[i] << endl;
+            }
+      }
+      else
+      {
+            cerr << "Error : Invalid index " << startIndex << " or " << endIndex << " Valid range is 0 to " << (size - 1) << endl;
+      }
 }
 void separatorLine()
 {
@@ -35,5 +46,9 @@ int main()
       gets(myArray, size);
       separatorLine();
       getByIndex(myArray, size, 9);
+      separatorLine();
+      getByIndex(myArray, size, 0);
+      separatorLine();
+      getValueStartIndexToEndIndex(myArray, size, 1, 2);
       return 0;
 }

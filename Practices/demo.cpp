ed020d496c75afea93f2887700139f7400993c61
TaiChi <anothai.0978452316@gmail.com>
2024-07-25 22:35:45 +0700

    feat : fn loop display value in array & address array

--- Practices/demo.cpp
+++ Practices/demo.cpp
@@ -2,6 +2,13 @@
 
 using namespace std;
 
+void display(int array[], int size)
+{
+      for (int i = 0; i < size; i++)
+      {
+            cout << array[i] << " : " << &array[i] << endl;
+      }
+}
 int main()
 {
       int myArray[5]{0, 1, 2, 3, 4};

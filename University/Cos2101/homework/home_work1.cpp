c00bfffa5db28fea8ac23e9e48207a27538eb9be
TaiChi <anothai.0978452316@gmail.com>
2024-08-03 00:04:45 +0700

    initial : define req , input , formula , output , and constant variable

---
+++ University/Cos2101/homework/home_work1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+      const double mile_to_kilometer = 1.60934;
+      double miles;
+      cout << "Input mile";
+      cin >> mile;
+      return 0;
+}
+/// convert mile to kilometer
+// input mile
+// formula kilometer = mile * 1.60934
+// output kilometer

faabb1cd8bb4a8d2bc6a3c6249349a2549fd0754
TaiChi <anothai.0978452316@gmail.com>
2024-08-04 21:03:40 +0700

    error : I don't know what happended cann't to push

--- University/Cos2101/homework/home_work1.cpp
+++ University/Cos2101/homework/home_work1.cpp
@@ -2,12 +2,25 @@
 
 using namespace std;
 
+void input(double &miles)
+{
+      cout << "Input mile : ";
+      cin >> miles;
+}
+double convert(double miles)
+{
+      const double mile_to_kilometer = 1.60934;
+      return miles * mile_to_kilometer;
+}
+void output(double kilometers)
+{
+      cout << "convert mile to kilometer : " << kilometers << endl;
+}
+
 int main()
 {
       const double mile_to_kilometer = 1.60934;
-      double miles;
-      cout << "Input mile";
-      cin >> mile;
+      double miles, kilometers;
       return 0;
 }
 /// convert mile to kilometer

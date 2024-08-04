8c6420e77b9c7da8e5f61f7a4cbf81dbfbdc2b98
TaiChi <anothai.0978452316@gmail.com>
2024-08-04 21:22:43 +0700

    feat : write basic code convert mile to km

--- Youtube/main.cpp
+++ Youtube/main.cpp
@@ -3,5 +3,10 @@
 using namespace std;
 
 int main(){
+    const float mile_to_km{1.60934};
+    float miles, km;
+    cout << "Enter the distance in miles: ";cin>>miles;
+    km = miles * mile_to_km;
+    cout << "The distance in km is: " << km << endl;
     return 0;
 }

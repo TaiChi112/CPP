5a95b42e3caf8974de23bb5dbbc4ea6cf60ee132
TaiChi <anothai.0978452316@gmail.com>
2024-08-10 00:11:48 +0700

    learn-feat : new fn update new data vs. old data

--- Others/main.cpp
+++ Others/main.cpp
@@ -11,21 +11,39 @@ void display_node(Node *node)
 {
     while (node != nullptr)
     {
-        cout << node->data << " -> ";
+        cout << node->data << " ";
         node = node->next;
     }
+    cout << endl;
 }
-Node *append(Node *node, int data){
+Node *append(Node *node, int data)
+{
     Node *newNode(new Node{data, nullptr});
-    if(node == nullptr){
+    if (node == nullptr)
+    {
         node = newNode;
         return node;
     }
     Node *temp = node;
-    while(temp->next != nullptr){
+    while (temp->next != nullptr)
+    {
         temp = temp->next;
     }
     temp->next = newNode;
+    return node;
+}
+Node *update(Node *node, int old_data, int new_data)
+{
+    Node *temp = node;
+    while (temp != nullptr)
+    {
+        if (temp->data == old_data)
+        {
+            temp->data = new_data;
+            return node;
+        }
+        temp = temp->next;
+    }
     return node;
 }
 
@@ -33,9 +51,13 @@ int main()
 {
     Node *node(new Node{112, nullptr});
     display_node(node);
+    cout<<"-------"<<endl;
     node = append(node, 123);
     node = append(node, 124);
     display_node(node);
-    
+    cout<<"-------"<<endl;
+    node = update(node, 123, 125);
+    node = update(node, 112, 999);
+    display_node(node);
     return 0;
 }

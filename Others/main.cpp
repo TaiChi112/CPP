fcc63d7a16ee0e3be9968ad54a601ba835c309e9
TaiChi <anothai.0978452316@gmail.com>
2024-08-10 00:05:48 +0700

    learn-feat : new fn display & append

--- Others/main.cpp
+++ Others/main.cpp
@@ -7,11 +7,35 @@ struct Node
     int data;
     Node *next;
 };
+void display_node(Node *node)
+{
+    while (node != nullptr)
+    {
+        cout << node->data << " -> ";
+        node = node->next;
+    }
+}
+Node *append(Node *node, int data){
+    Node *newNode(new Node{data, nullptr});
+    if(node == nullptr){
+        node = newNode;
+        return node;
+    }
+    Node *temp = node;
+    while(temp->next != nullptr){
+        temp = temp->next;
+    }
+    temp->next = newNode;
+    return node;
+}
 
 int main()
 {
-    Node *head(new Node{112, nullptr});
-    cout << head->data << endl;
-    cout << head->next << endl;
+    Node *node(new Node{112, nullptr});
+    display_node(node);
+    node = append(node, 123);
+    node = append(node, 124);
+    display_node(node);
+    
     return 0;
 }

a61d1620e59c7963f66518d29b2d8fa5b8db250d
TaiChi <anothai.0978452316@gmail.com>
2024-08-16 23:18:33 +0700

    learn : array with DS linked list

--- University/Cos2101/demo/demo2.cpp
+++ University/Cos2101/demo/demo2.cpp
@@ -10,36 +10,45 @@ struct Student
 
 int main()
 {
-    Student *head = NULL;
-    Student *current = NULL;
-    Student *last = NULL;
-    Student *temp = NULL;
+    Student *head[]{new Student{"1", NULL}, new Student{"2", NULL}, new Student{"3", NULL}, new Student{"4", NULL}, new Student{"5", NULL}};
 
     for (int i = 0; i < 5; i++)
     {
-        temp = new Student;
-        cin >> temp->name;
-        temp->next = NULL;
-        if (head == NULL)
-        {
-            head = temp;
-            last = temp;
-        }
-        else
-        {
-            last->next = temp;
-            last = temp;
-        }
+        cout << head[i]->name << endl;
+        head[i]->next = head[i + 1];
     }
+    return 1;
 
-    current = head;
+    // Student *head = NULL;
+    // Student *current = NULL;
+    // Student *last = NULL;
+    // Student *temp = NULL;
 
-    while (current != NULL)
-    {
-        cout << current->name << " -> ";
-        current = current->next;
-    }
+    // for (int i = 0; i < 5; i++)
+    // {
+    //     temp = new Student;
+    //     cin >> temp->name;
+    //     temp->next = NULL;
+    //     if (head == NULL)
+    //     {
+    //         head = temp;
+    //         last = temp;
+    //     }
+    //     else
+    //     {
+    //         last->next = temp;
+    //         last = temp;
+    //     }
+    // }
 
-    cout << "null" << endl;
+    // current = head;
+
+    // while (current != NULL)
+    // {
+    //     cout << current->name << " -> ";
+    //     current = current->next;
+    // }
+
+    // cout << "null" << endl;
     return 0;
 }

964b014267dac436931306255888e19cdaccddd3
TaiChi <anothai.0978452316@gmail.com>
2024-08-27 23:43:36 +0700

    learn : class in C++

--- University/Cos2102/main.cpp
+++ University/Cos2102/main.cpp
@@ -2,16 +2,28 @@
 
 using namespace std;
 
-struct Node
+struct Node2
 {
       int data;
 };
+class Node3
+{
+      int data;
 
+public:
+      Node3(int data) : data(data) {}
+      int get_data()
+      {
+            return data;
+      }
+};
 int main()
 {
       int node1(112);
       cout << node1 << endl;
-      Node node2{112};
+      Node2 node2{112};
       cout << node2.data << endl;
+      Node3 node{113};
+      cout << node.get_data() << endl;
       return 0;
 }

4d58a3bcb63d03c50bb48e0136f5c994552a599d
TaiChi <anothai.0978452316@gmail.com>
2024-09-17 22:24:13 +0700

    chore : add attribute food meat & vegetable

--- University/Cos2102/learn/mid28.cpp
+++ University/Cos2102/learn/mid28.cpp
@@ -3,11 +3,18 @@ using namespace std;
 
 class Food
 {
+    double price;
+    int meatNum;
+    int vegNum;
+    Meat *meat;
+    Vegetable *veg;
 };
 class Meat
 {
+    int type;
 };
 
 class Vegetable
 {
+    int type;
 };

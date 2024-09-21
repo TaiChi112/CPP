d1a3eb4fdaa1991bc0f4a12016bb8ec6cfad38b9
TaiChi <anothai.0978452316@gmail.com>
2024-09-21 22:27:16 +0700

    learn : oop with cpp

---
+++ University/Cos2102/learn/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+class Person
+{
+    int height;
+    int weight;
+
+public:
+    Person():height(0),weight(0) {}
+    Person(int h, int w) : height(h), weight(w) {}
+    void show()
+    {
+        cout << "This person is height  : " << height << " weight : " << weight << endl;
+    }
+};
+int main()
+{
+    Person p1;
+    p1.show();
+    Person p2(180, 80);
+    p2.show();
+    return 0;
+}

13b697701c279b9f6846c3265ab71a580661e187
TaiChi <anothai.0978452316@gmail.com>
2024-09-22 21:35:39 +0700

    Do university OOP questions.

--- University/Cos2102/mid28/main.cpp
+++ University/Cos2102/mid28/main.cpp
@@ -1,15 +1,26 @@
 #include <iostream>
 #include "food.cpp"
+// #include "meat.cpp"
+// #include "vegetable.cpp"
 
 using namespace std;
 
 int main()
 {
-    Food f1;
-    f1.show();
-    Meat m1;
-    m1.show();
-    Vegetable v1;
-    v1.show();
+    Food f1,f2;
+    f1.setPrice(100);
+    f1.showPrice();
+    f1.setMeat(new Meat());
+    f1.getMeat()->setType(1);
+    f1.getMeat()->showType();
+    f2.showPrice();
+
+    // Meat m;
+    // m.setType(0);
+    // m.showType();
+
+    // Vegetable v;
+    // v.setName("Potato");
+    // v.showName();
     return 0;
 }

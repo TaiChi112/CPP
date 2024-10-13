20a87917a8b714a69da798ab22b496276f8fdfc3
TaiChi <anothai.0978452316@gmail.com>
2024-10-13 20:35:56 +0700

    comment : code

--- University/Cos2102/learn/main.cpp
+++ University/Cos2102/learn/main.cpp
@@ -1,27 +1,21 @@
-#include <iostream>
 
-using namespace std;
+// #include <iostream>
 
-class Person
-{
-    int height;
-    int weight;
+// using namespace std;
+// // association
+// class Student
+// {
+//     string name;
 
-public:
-    Person():height(0),weight(0) {}
-    Person(int h, int w) : height(h), weight(w) {}
-    void show()
-    {
-        cout << "This person is height  : " << height << " weight : " << weight << endl;
-    }
-};
-int main()
-{
-    Person p1;
-    p1.show();
-    Person p2(180, 80);
-    p2.show();
-    Person p3(160, 50);
-    p3.show();
-    return 0;
-}
+// public:
+// };
+// class Course
+// {
+//     string name;
+
+// public:
+// };
+// int main()
+// {
+//     return 0;
+// }

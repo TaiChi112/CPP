573b64a1612c9b31329e51b9ccf379f362c84ada
TaiChi <anothai.0978452316@gmail.com>
2024-12-08 02:08:04 +0700

    desc : actually I'm very lazy explain What am I doing

--- _proposition/chapter22/main.cpp
+++ _proposition/chapter22/main.cpp
@@ -17,7 +17,8 @@ public:
 };
 class ElectricPencilSharpener : public PencilSharpener
 {
-    int type; //
+    int type;
+
 public:
     ElectricPencilSharpener();
     ElectricPencilSharpener(int, int, double, int);
@@ -26,9 +27,9 @@ public:
 int main()
 {
     cout << "Create PencilSharpener object \n";
-    int color, channel;
+    int color, channel, type;
     double price;
-    cout << "enter PencilSharpener color : ";
+    cout << "Enter PencilSharpener color : ";
 
     while (color < 1 || color > 5)
     {
@@ -36,65 +37,114 @@ int main()
         if (color < 1 || color > 5)
         {
             cout << "color must be between 1 and 5 \n";
-            cout << "enter PencilSharpener color : ";
+            cout << "Enter PencilSharpener color : ";
         }
     }
-
-    cout << "enter PencilSharpener channel : ";
+    cout << "Enter PencilSharpener channel : ";
     cin >> channel;
     cout << "enter PencilSharpener price : ";
     cin >> price;
+    cout << "Enter PencilSharpener type : ";
+    cin >> type;
+    cout << endl;
     PencilSharpener p1, p2(color, channel, price);
+    cout << "PencilSharpener 1" << endl;
     p1.show();
+    cout << endl;
+    cout << "PencilSharpener 2" << endl;
     p2.show();
+
+    ElectricPencilSharpener e1, e2(color, channel, price, type);
+    cout << endl;
+    cout << "EletricPencilSharpener 1" << endl;
+    e1.show();
+    cout << endl;
+    cout << "EletricPencilSharpener 2" << endl;
+    e2.show();
+
     return 0;
 }
 PencilSharpener::PencilSharpener() : color(1), num(3), price(112.5) {}
-PencilSharpener::PencilSharpener(int c, int num, double p)
-{
-    if (c >= 1 && c <= 5)
-    {
-        this->color = c;
-    }
-    if (num <= 0)
-    {
-        this->num = num;
-    }
-    if (price < 0)
-    {
-        this->price = 112;
-    }
-    else
-    {
-        this->price = p;
-    }
-}
+PencilSharpener::PencilSharpener(int c, int num, double p) : color((c >= 1 && c <= 5) ? c : 1), num((num > 0) ? num : 3), price((p > 0) ? p : 112.5) {}
+
 void PencilSharpener::show()
 {
+    switch (this->getColor())
+    {
+    case 1:
+        cout << "color : You have (Blue) pencil sharpener" << endl;
+        break;
+    case 2:
+        cout << "color : You have (White) pencil sharpener" << endl;
+        break;
+    case 3:
+        cout << "color : You have (Green) pencil sharpener" << endl;
+        break;
+    case 4:
+        cout << "color : You have (Pink) pencil sharpener" << endl;
+        break;
+    case 5:
+        cout << "color : You have (Black) pencil sharpener" << endl;
+        break;
 
-    cout << "color: " << color << endl;
-    cout << "num: " << num << endl;
-    cout << "price: " << price << endl;
+    default:
+        cout << "color : You have (Blue) pencil sharpener" << endl;
+        break;
+    }
+    switch (this->getNum())
+    {
+    case 1:
+        cout << "channel : You select pencil 1 channel" << endl;
+        break;
+    case 2:
+        cout << "channel : You select pencil 2 channel" << endl;
+        break;
+    case 3:
+        cout << "channel : You select pencil 3 channel" << endl;
+        break;
+    default:
+        cout << "channel : You select pencil 1 channel" << endl;
+        break;
+    }
+    cout << "price: " << this->getPrice() << "$" << endl;
 }
 int PencilSharpener::getColor()
 {
-    return color;
+    return this->color;
 }
 int PencilSharpener::getNum()
 {
-    return num;
+    return this->num;
 }
 double PencilSharpener::getPrice()
 {
-    return price;
+    return this->price;
 }
 
-ElectricPencilSharpener::ElectricPencilSharpener() : PencilSharpener(), type(1) {}
-ElectricPencilSharpener::ElectricPencilSharpener(int c, int num, double p, int t) : PencilSharpener(c, num, p), type(t) {}
+ElectricPencilSharpener::ElectricPencilSharpener() : PencilSharpener(2, 1, 280), type(1) {}
+ElectricPencilSharpener::ElectricPencilSharpener(int c, int num, double p, int t) : PencilSharpener(c, num, p), type(t)
+{
+    if (type < 1 || type > 3)
+    {
+        this->type = t;
+    }
+}
 void ElectricPencilSharpener::show()
 {
-    cout << "color: " << getColor() << endl;
-    cout << "num: " << getNum() << endl;
-    cout << "price: " << getPrice() << endl;
-    cout << "type: " << type << endl;
+    PencilSharpener::show();
+    switch (type)
+    {
+    case 1:
+        cout << "type: use plug-in" << endl;
+        break;
+    case 2:
+        cout << "type: use battery" << endl;
+        break;
+    case 3:
+        cout << "type: use charging USB " << endl;
+        break;
+
+    default:
+        break;
+    }
 }

31c3d42eccce5b806d8c4b98d7d94edfd54fc645
TaiChi <anothai.0978452316@gmail.com>
2024-12-12 20:50:28 +0700

    module : many module object class in the test core cpp

---
+++ Books/chapter2/address.h
@@ -0,0 +1,106 @@
+#ifndef ADDRESS_H
+#define ADDRESS_H
+#include <iostream>
+using namespace std;
+class Birthday
+{
+    int day, month, year;
+
+public:
+    Birthday();
+    Birthday(int, int, int);
+    int getDay();
+    int getMonth();
+    int getYear();
+};
+
+class IdentityCard
+{
+    int id;
+    string firstname;
+    string lastname;
+    Birthday birthday;
+
+public:
+    IdentityCard();
+    IdentityCard(int, string, string, Birthday);
+    int getId();
+    string getFirstname();
+    string getLastname();
+    Birthday getBirthday();
+};
+class Address
+{
+    IdentityCard identityCard;
+    string street;
+    string city;
+    string country;
+
+public:
+    Address();
+    Address(IdentityCard, string, string, string);
+    string getStreet();
+    string getCity();
+    string getCountry();
+    void display();
+};
+Birthday::Birthday() : day(0), month(0), year(0) {}
+Birthday::Birthday(int d, int m, int y) : day((d <= 31 ? d : 1)), month((m <= 12 ? m : 1)), year(y) {}
+int Birthday::getDay() { return day; };
+int Birthday::getMonth() { return month; };
+int Birthday::getYear() { return year; };
+IdentityCard::IdentityCard() : birthday(), id(0), firstname(""), lastname("") {}
+IdentityCard::IdentityCard(int id, string fn, string ln, Birthday bd) : id(id), firstname(fn), lastname(ln), birthday(bd) {}
+int IdentityCard::getId() { return id; }
+string IdentityCard::getFirstname() { return firstname; }
+string IdentityCard::getLastname() { return lastname; }
+Birthday IdentityCard::getBirthday() { return birthday; }
+Address::Address() : identityCard(), street(""), city(""), country("") {}
+Address::Address(IdentityCard idCard, string st, string c, string co) : identityCard(idCard), street(st), city(c), country(co) {}
+string Address::getStreet() { return street; }
+string Address::getCity() { return city; }
+string Address::getCountry() { return country; }
+void Address::display()
+{
+    cout << "Identity Card Information: " << endl;
+    cout << "ID: " << identityCard.getId() << endl;
+    cout << "Name: " << identityCard.getFirstname() << " " << identityCard.getLastname() << endl;
+    cout << "Birthday: " << identityCard.getBirthday().getDay() << "/" << identityCard.getBirthday().getMonth() << "/" << identityCard.getBirthday().getYear() << endl;
+    cout << "Address Information: " << endl;
+    cout << "Street: " << street << endl;
+    cout << "City: " << city << endl;
+    cout << "Country: " << country << endl;
+}
+
+void IOBirthday(int &day, int &month, int &year)
+{
+    cout << "Enter your birthday (dd mm yyyy)\n";
+    cout << "day : ";
+    cin >> day;
+    cout << "month : ";
+    cin >> month;
+    cout << "year : ";
+    cin >> year;
+}
+void IOIdentityCard(int &id, string &firstname, string &lastname)
+{
+    cout << "Enter your identity card information (id firstname lastname)\n";
+    cout << "id : ";
+    cin >> id;
+    cout << "firstname : ";
+    cin >> firstname;
+    cout << "lastname : ";
+    cin >> lastname;
+}
+void IOAddress(string &street, string &city, string &country)
+{
+    cout << "Enter your address information (street city country)\n";
+    cout << "street : ";
+    cin >> street;
+    cout << "city : ";
+    cin >> city;
+    cout << "country : ";
+    cin >> country;
+}
+
+#endif

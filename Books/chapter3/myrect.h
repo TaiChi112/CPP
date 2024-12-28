6a5361ba1aab6a73bac013d7db1effbc841b9da4
TaiChi <anothai.0978452316@gmail.com>
2024-12-29 03:16:59 +0700

    make : do homework chapter 4

--- Books/chapter3/myrect.h
+++ Books/chapter3/myrect.h
@@ -1,37 +1,9 @@
 #ifndef MYRECT_H
 #define MYRECT_H
 #include <iostream>
+#include "../chapter2/rectangle.h"
 using namespace std;
-class Rectangle
-{
-    int width, length;
-    static int count;
 
-public:
-    Rectangle() : width(0), length(0) { count++; }
-    Rectangle(int w, int l) : width(w), length(l) { count++; }
-    ~Rectangle() { count--; }
-
-    void set(int w, int l)
-    {
-        width = w;
-        length = l;
-    }
-    int getWidth() { return width; }
-    int getLength() { return length; }
-    int areaRectangle() { return width * length; }
-    int circumference() { return 2 * (width + length); }
-    void show()
-    {
-        cout << "Rectangle: " << "Width = " << width
-             << ", Length = " << length
-             << ", Area = " << areaRectangle()
-             << ", Circumference = " << circumference() << endl;
-    }
-
-    static int getCount() { return count; }
-};
-int Rectangle::count = 0;
 class MyRect
 {
     Rectangle **rects;
@@ -90,7 +62,6 @@ void MyRect::show()
         {
             cout << "Reatangle at (" << i << ", " << j << "): " << endl;
             rects[i][j].show();
-
         }
     }
 }

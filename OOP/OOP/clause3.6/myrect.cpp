216bc5410de54e24753081dc84c168611cfda9a1
TaiChi <anothai.0978452316@gmail.com>
2024-12-29 20:04:04 +0700

    test : method in the class MyRectangle

--- OOP/OOP/clause3.6/myrect.cpp
+++ OOP/OOP/clause3.6/myrect.cpp
@@ -5,30 +5,33 @@ using namespace std;
 
 int main()
 {
-    MyRect mr(2, 3);
-    mr.setRect(0, 0, 3, 4); // set Rectangle at index col(0), row(0) with width(3), height(4)
-    mr.setRect(0, 1, 5, 6);
-    mr.setRect(0, 2, 7, 8);
-    mr.setRect(1, 0, 9, 10);
-    mr.setRect(1, 1, 11, 12);
-    mr.setRect(1, 2, 13, 14);
+    MyRect mr2(2, 3);
+    mr2.setRect(0, 0, 3, 4); // set Rectangle at index col(0), row(0) with width(3), height(4)
+    mr2.setRect(0, 1, 5, 6);
+    mr2.setRect(0, 2, 7, 8);
+    mr2.setRect(1, 0, 9, 10);
+    mr2.setRect(1, 1, 11, 12);
+    mr2.setRect(1, 2, 13, 14);
     cout << endl;
-    mr.show();
+    mr2.show();
 
     cout << endl;
-    cout << "Try to get myReact" << endl;
-    mr.getRect(0, 0).show();
+    cout << "Try to get my Rectangle at index : " << endl;
+    mr2.getRect(1, 0).show();
+    cout << endl;
+    cout << "try to get Rows of object mr2 : " << mr2.getRow() << endl;
+    cout << "try to get Cols of object mr2 : " << mr2.getCol() << endl;
 
     cout << endl;
     cout << "Try to reset array myRect to 2 row and 2 col" << endl;
     cout << endl;
-    mr.reset(2, 2); // reset array to 2 row and 2 col
-    mr.setRect(0, 0, 3, 4);
-    mr.setRect(0, 1, 5, 6);
-    mr.setRect(1, 0, 7, 8);
-    mr.setRect(1, 1, 9, 10);
+    mr2.reset(2, 2); // reset array to 2 row and 2 col
+    mr2.setRect(0, 0, 3, 4);
+    mr2.setRect(0, 1, 5, 6);
+    mr2.setRect(1, 0, 7, 8);
+    mr2.setRect(1, 1, 9, 10);
     cout << endl;
-    mr.show();
+    mr2.show();
     cout << endl;
 
     return 0;

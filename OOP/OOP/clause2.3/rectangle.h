8186b70bb49bf84a6c02966e122ddeab7d9ad9f5
TaiChi <anothai.0978452316@gmail.com>
2024-12-29 20:02:44 +0700

    refactor : normalization implement code

--- OOP/OOP/clause2.3/rectangle.h
+++ OOP/OOP/clause2.3/rectangle.h
@@ -12,10 +12,14 @@ public:
     Rectangle(int, int);
     ~Rectangle();
     void set(int, int);
+    void setWidth(int);
+    void setLength(int);
+    int findCircumference(int, int);
+    int findArea(int, int);
     int getWidth();
     int getLength();
-    int areaReatangle(int, int);
-    int circumference(int, int);
+    int getArea();
+    int getCircumference();
     void show();
     static int getCount();
 };
@@ -35,15 +39,19 @@ void Rectangle::set(int w, int l)
     width = w;
     length = l;
 }
+void Rectangle::setWidth(int w) { width = w; }
+void Rectangle::setLength(int l) { length = l; }
 int Rectangle::getWidth() { return width; }
 int Rectangle::getLength() { return length; }
-int Rectangle::areaReatangle(int w, int l) { return w * l; }
-int Rectangle::circumference(int w, int l) { return 2 * (w + l); }
+int Rectangle::findArea(int w, int l) { return w * l; }
+int Rectangle::findCircumference(int w, int l) { return 2 * (w + l); }
+int Rectangle::getArea() { return findArea(width, length); }
+int Rectangle::getCircumference() { return findCircumference(width, length); }
 void Rectangle::show()
 {
-    cout << "Rectangle : (" << getWidth() << ", " << getLength() << ")\n";
-    cout << "Area : " << areaReatangle(getWidth(), getLength()) << endl;
-    cout << "Border : " << circumference(getWidth(), getLength()) << endl;
+    cout << "Rectangle : (" << getWidth() << ", " << getLength() << ") ";
+    cout << "Area : " << findArea(getWidth(), getLength()) << " : ";
+    cout << "Border : " << findCircumference(getWidth(), getLength()) << endl;
 }
 int Rectangle::count = 0;
 int Rectangle::getCount() { return count; }

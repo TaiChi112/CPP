c5c0fdf583a5b508552805e8dce24207a8d7dde2
TaiChi <anothai.0978452316@gmail.com>
2024-12-29 19:13:31 +0700

    refactor : test to use all method in the class Point

--- OOP/OOP/clause2.1/point.cpp
+++ OOP/OOP/clause2.1/point.cpp
@@ -5,16 +5,30 @@ using namespace std;
 
 int main()
 {
-    // copy this CLI for run code -> g++ point.cpp -o point.exe && ./point.exe
     string name("A");
     double x(4.0), y(5.0);
 
+    // create Object of Point
     Point p1, p2(name, x, y);
+
     cout << endl;
     p1.show();
     cout << endl;
     p2.show();
     cout << endl;
+
+    // get attribute of object p2
+
+    cout << "get attribute Name of object p1 : " << p2.getName() << endl;
+    cout << "get attribute X of object p1 : " << p2.getX() << endl;
+    cout << "get attribute Y of object p1 : " << p2.getY() << endl;
+
+    // set attribute of object p1
+
+    p1.setName("B");
+    p1.setX(6.0);
+    p1.setY(7.0);
+    p1.show();
 
     return 0;
 }

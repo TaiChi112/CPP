7eb1829390b9db951887056a74e4afaaffaa9e4e
TaiChi <anothai.0978452316@gmail.com>
2024-12-29 19:22:59 +0700

    refactor : nothin much adding method get amount of object Point

--- OOP/OOP/clause2.2/point.cpp
+++ OOP/OOP/clause2.2/point.cpp
@@ -5,18 +5,36 @@ using namespace std;
 
 int main()
 {
-    // copy this CLI for run code -> g++ point.cpp -o point.exe && ./point.exe
     string name("A");
     double x(4.0), y(5.0);
 
+    // create Object of Point
     Point p1, p2(name, x, y);
+
     cout << endl;
     p1.show();
     cout << endl;
     p2.show();
     cout << endl;
-    cout << "amount object : " << Point::getCount() << endl;
+
+    // get attribute of object p2
+
+    cout << "get attribute Name of object p1 : " << p2.getName() << endl;
+    cout << "get attribute X of object p1 : " << p2.getX() << endl;
+    cout << "get attribute Y of object p1 : " << p2.getY() << endl;
     cout << endl;
 
+    // set attribute of object p1
+
+    cout << "try to set attribute of object p1 : " << endl;
+    cout << endl;
+    p1.setName("B");
+    p1.setX(6.0);
+    p1.setY(7.0);
+    p1.show();
+    cout << endl;
+
+    cout << "get amount of oject : " << Point::getCount() << endl;
+    cout << endl;
     return 0;
 }

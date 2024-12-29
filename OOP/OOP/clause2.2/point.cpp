17a54cb9ee182cc632d650348a04960848c20bf2
TaiChi <anothai.0978452316@gmail.com>
2024-12-29 22:05:01 +0700

    test : try to get amount object before create object Point

--- OOP/OOP/clause2.2/point.cpp
+++ OOP/OOP/clause2.2/point.cpp
@@ -5,8 +5,10 @@ using namespace std;
 
 int main()
 {
-    string name("A");
+    string name("B");
     double x(4.0), y(5.0);
+
+    cout << "get amount of object before create object : " << Point::getCount() << endl;
 
     // create Object of Point
     Point p1, p2(name, x, y);
@@ -19,22 +21,22 @@ int main()
 
     // get attribute of object p2
 
-    cout << "get attribute Name of object p1 : " << p2.getName() << endl;
-    cout << "get attribute X of object p1 : " << p2.getX() << endl;
-    cout << "get attribute Y of object p1 : " << p2.getY() << endl;
+    cout << "get attribute Name of object p2 : " << p2.getName() << endl;
+    cout << "get attribute X of object p2 : " << p2.getX() << endl;
+    cout << "get attribute Y of object p2 : " << p2.getY() << endl;
     cout << endl;
 
     // set attribute of object p1
 
     cout << "try to set attribute of object p1 : " << endl;
     cout << endl;
-    p1.setName("B");
+    p1.setName("A");
     p1.setX(6.0);
     p1.setY(7.0);
     p1.show();
     cout << endl;
 
-    cout << "get amount of oject : " << Point::getCount() << endl;
+    cout << "get amount of object : " << Point::getCount() << endl;
     cout << endl;
     return 0;
 }

5bd7ca0c560fe345bb016a913199ef06118208c8
TaiChi <anothai.0978452316@gmail.com>
2024-12-30 14:52:28 +0700

    test : add method of object Address

--- OOP/OOP/clause6.6/address.cpp
+++ OOP/OOP/clause6.6/address.cpp
@@ -6,8 +6,24 @@ using namespace std;
 int main()
 {
     string street("Ramkhamheang"), city("Bangkok"), country("Thailand");
-    Address ad2(street, city, country);
+    Address ad1, ad2(street, city, country);
+    ad1.show();
     cout << endl;
-    ad2.display();
+    ad2.show();
+    cout << endl;
+
+    cout << "Try to set new Address for ad1 object" << endl;
+    cout << endl;
+    ad1.set("Sukhumvit", "Bangkok", "Thailand");
+    ad1.show();
+    cout << endl;
+
+    cout << "Try to get attribute of ad1 object" << endl;
+    cout << endl;
+    cout << "Street: " << ad1.getStreet() << endl;
+    cout << "City: " << ad1.getCity() << endl;
+    cout << "Country: " << ad1.getCountry() << endl;
+    cout << endl;
+
     return 0;
 }

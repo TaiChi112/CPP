708429b7055b5a2030be9698d69a444bbc8cd39e
TaiChi <anothai.0978452316@gmail.com>
2024-12-30 17:07:28 +0700

    test : new method to test successs

--- OOP/OOP/clause3.6/myrect.cpp
+++ OOP/OOP/clause3.6/myrect.cpp
@@ -5,35 +5,46 @@ using namespace std;
 
 int main()
 {
-    MyRect mr2(2, 3);
-    mr2.setRect(0, 0, 3, 4); // set Rectangle at index col(0), row(0) with width(3), height(4)
-    mr2.setRect(0, 1, 5, 6);
-    mr2.setRect(0, 2, 7, 8);
-    mr2.setRect(1, 0, 9, 10);
-    mr2.setRect(1, 1, 11, 12);
-    mr2.setRect(1, 2, 13, 14);
+    int row(2), col(2);
+    MyRect mr(row, col);
+    int width, length;
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            cout << "Enter data for rectangle at position  " << i << " " << j << endl;
+            cout << "Enter width : ";
+            cin >> width;
+            cout << "Enter length : ";
+            cin >> length;
+            Rectangle rect(width, length);
+            mr.setRect(i, j, rect);
+        }
+    }
     cout << endl;
-    mr2.show();
-    cout << endl;
-
-    cout << "Try to get my Rectangle at index : " << endl;
-    mr2.getRect(1, 0).show();
-    cout << endl;
-
-    cout << "try to get Rows of object mr2 : " << mr2.getRow() << endl;
-    cout << "try to get Cols of object mr2 : " << mr2.getCol() << endl;
-    cout << endl;
-    
-    cout << "Try to reset array myRect to 2 row and 2 col" << endl;
-    cout << endl;
-    mr2.reset(2, 2); // reset array to 2 row and 2 col
-    mr2.setRect(0, 0, 3, 4);
-    mr2.setRect(0, 1, 5, 6);
-    mr2.setRect(1, 0, 7, 8);
-    mr2.setRect(1, 1, 9, 10);
-    cout << endl;
-    mr2.show();
+    mr.show();
     cout << endl;
 
+    cout << "Try to reset My Rectangle" << endl;
+    cout << endl;
+    int newRow(1), newCol(1);
+
+    mr.reset(newRow, newCol);
+    for (int i = 0; i < newRow; i++)
+    {
+        for (int j = 0; j < newCol; j++)
+        {
+            cout << "Enter data for rectangle at position  " << i << " " << j << endl;
+            cout << "Enter width : ";
+            cin >> width;
+            cout << "Enter length : ";
+            cin >> length;
+            Rectangle rect(width, length);
+            mr.setRect(i, j, rect);
+        }
+    }
+    cout << endl;
+    mr.show();
+    cout << endl;
     return 0;
 }

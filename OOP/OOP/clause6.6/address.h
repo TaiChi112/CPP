54e94e2acbf57e396c69469ad1f7ead487c79017
TaiChi <anothai.0978452316@gmail.com>
2024-12-30 14:52:06 +0700

    refactor : actually I forgot what I change

--- OOP/OOP/clause6.6/address.h
+++ OOP/OOP/clause6.6/address.h
@@ -13,28 +13,28 @@ public:
     Address();
     Address(string, string, string);
     ~Address();
+    void set(string, string, string);
     string getStreet();
     string getCity();
     string getCountry();
-    void display();
     void show();
     void IAddress();
 };
 Address::Address() : street("undefind"), city("undefind"), country("undefind") {}
-Address::Address(string st, string c, string co) : street(st), city(c), country(co) {} // #
+Address::Address(string st, string c, string co) : street(st), city(c), country(co) {}
 Address::~Address() { cout << "Desctructuring Address : " << getStreet() << " : " << getCity() << " : " << getCountry() << endl; }
 string Address::getStreet() { return street; }
 string Address::getCity() { return city; }
 string Address::getCountry() { return country; }
-void Address::display()
+void Address::set(string st, string c, string co)
 {
-    cout << "Address Information: " << endl;
-    cout << "Street: " << getStreet() << endl;
-    cout << "City: " << getCity() << endl;
-    cout << "Country: " << getCountry() << endl;
+    street = st;
+    city = c;
+    country = co;
 }
 void Address::show()
 {
+    cout << "Address Information: " << endl;
     cout << "Street: " << getStreet() << endl;
     cout << "City: " << getCity() << endl;
     cout << "Country: " << getCountry() << endl;

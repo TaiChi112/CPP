b89dbd7e4a0055ba2f4d5e7104f31168edd75b2b
TaiChi <anothai.0978452316@gmail.com>
2025-01-10 20:43:01 +0700

    feat : create prototype 3 class for use in the main

---
+++ OOP/mid/mid21/prototype.h
@@ -0,0 +1,49 @@
+#ifndef PROTOTYPE_H
+#define PROTOTYPE_H
+#include <iostream>
+class Dessert
+{
+    int price;
+
+public:
+    Dessert();
+    Dessert(int);
+    ~Dessert();
+    void show();
+    int getPrice();
+    void setPrice(int);
+};
+class Cake : public Dessert
+{
+    int flavor;
+    int type;
+    bool topping;
+
+public:
+    Cake();
+    Cake(int, int, bool, int);
+    ~Cake();
+    void show();
+    int getFlavor();
+    int getType();
+    bool getTopping();
+    void setFlavor(int);
+    void setType(int);
+    void setTopping(bool);
+};
+class IceCream : public Dessert
+{
+    int flavor;
+    bool topping;
+
+public:
+    IceCream();
+    IceCream(int, bool, int);
+    ~IceCream();
+    void show();
+    int getFlavor();
+    bool getTopping();
+    void setFlavor(int);
+    void setTopping(bool);
+};
+#endif // PROTOTYPE_H

c4b3ec10ac35683fb78cfe44e3f00472b5dd12b4
TaiChi112 <anothai.0978452316@gmail.com>
2025-03-23 23:12:41 +0700

    refactor: dynamic array with vector std use the case 1D & 2D

--- DSA/Array/array_feat_o1/main.cpp
+++ DSA/Array/array_feat_o1/main.cpp
@@ -1,7 +1,18 @@
 #include <iostream>
 #include <vector>
 using namespace std;
+class Person
+{
+    int id;
+    string name;
 
+public:
+    Person(int _id, string _name) : id(_id), name(_name) {}
+    void display()
+    {
+        cout << "id: " << id << " name: " << name << endl;
+    }
+};
 int main()
 {
     int *arr = new int[5]{1, 2, 3, 4, 5};
@@ -27,6 +38,42 @@ int main()
         {
             cout << "row " << i << " col " << j << ": " << arr2[i][j] << endl;
         }
+    }
+    for (int i = 0; i < rows; i++)
+    {
+        delete[] arr2[i];
+    }
+    delete[] arr2;
+
+    vector<int> arr3 = {1, 2, 3, 4, 5};
+    for (int i = 0; i < arr3.size(); i++)
+    {
+        cout << arr3[i] << endl;
+    }
+    vector<vector<int>> arr4 = {{112, 211}, {314, 415}};
+    for (int i = 0; i < arr4.size(); i++)
+    {
+        for (int j = 0; j < arr4[i].size(); j++)
+        {
+            cout << "row " << i << " col " << j << ": " << arr4[i][j] << endl;
+        }
+    }
+    vector<vector<int>> arr5(rows, vector<int>(cols, 0));
+    int datas2 = 112;
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            arr5[i][j] = datas2++;
+        }
+    }
+    for (const auto &row : arr5)
+    {
+        for (int data : row)
+        {
+            cout << data << " ";
+        }
+        cout << endl;
     }
     return 0;
 }

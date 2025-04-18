41ef9801cc510e60b8fc328b9db072697e8f246f
Anothai Vichapaiboon <110210626+TaiChi112@users.noreply.github.com>
2025-04-18 11:55:53 +0700

    Update main.cpp

--- gh-csp/main.cpp
+++ gh-csp/main.cpp
@@ -1,48 +1,50 @@
 #include <iostream>
-
 using namespace std;
-class Stove{
-    int type;// 1 gas 2 eletric 3 Electromagnet 4 infarate
-    public:
-    Stove(): type(1){}
-    Stove(int t):type(t){}
-    ~Stove(){}
-        
+
+class Stove {
+    int type; // 1 gas, 2 electric, 3 electromagnetic, 4 infrared
+public:
+    Stove() : type(1) {}
+    Stove(int t) : type(t) {}
+    ~Stove() {}
 };
-class Pot{
+
+class Pot {
     double price;
-    int width,height,num;
+    int width, height, num;
     Stove *stove;
-    public:
-    Pot():price(299),width(18),height(8),num(1){
+public:
+    Pot() : price(299), width(18), height(8), num(1) {
         stove = new Stove[num];
         stove[0] = Stove(1);
     }
-    Pot(double p,int w,int h, int n): price(p), width(w), height(h),num(n){
-        stove = new Stove[num]
+    Pot(double p, int w, int h, int n) : price(p), width(w), height(h), num(n) {
+        stove = new Stove[num];
+        for (int i = 0; i < num; ++i) {
+            stove[i] = Stove(1); // Default to gas stove type
+        }
     }
-    ~Pot(){}
-        
+    ~Pot() {
+        delete[] stove; // Proper cleanup of dynamically allocated memory
+    }
 };
-class SteamingPot:public Pot{
+
+class SteamingPot : public Pot {
     int layer;
-    public:
-    SteamingPot():layer(1),Pot(450,25,30,1){
-        Stove *stove = new Stove[1];
-        stove[0] = new Stove(1);
-    }
-    SteamingPot(int ly): layer(ly), Pot() {}
-    ~SteamingPot(){}
-};  
-class NoodlePot:public Pot{
-    int compartment;
-    public:
-    NoodlePot(int cpmt): compartment(cpmt) {}
-    ~NoodlePot(){}
-    
-        
+public:
+    SteamingPot() : Pot(450, 25, 30, 1), layer(1) {}
+    SteamingPot(int ly) : Pot(), layer(ly) {}            
+    ~SteamingPot() {}
 };
-int main(){
+
+class NoodlePot : public Pot {
+    int compartment;
+public:
+    NoodlePot(int cpmt) : Pot(), compartment(cpmt) {}
+    ~NoodlePot() {}
+};
+
+int main() {
     cout << "Hello, World!" << endl;
     return 0;
-}    
+}.  

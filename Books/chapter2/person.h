a0ffc640cb040d50c4d7e995c16766fb953605ef
TaiChi <anothai.0978452316@gmail.com>
2025-05-19 01:35:03 +0700

    rfc: change anymore code I lazy explain

--- Books/chapter2/person.h
+++ Books/chapter2/person.h
@@ -1,6 +1,8 @@
 #ifndef PERSON_H
 #define PERSON_H
 #include <iostream>
+#include "../chapter2/date.h"
+#include "../chapter2/address.h"
 using namespace std;
 class Person
 {
@@ -8,50 +10,73 @@ class Person
     int id;
     string firstname;
     string lastname;
-    string date;
+    Date date;
+    Address address;
 
 public:
     Person();
-    Person(int, string, string, string);
+    Person(int, string, string, Date, Address);
     ~Person();
-    void set(int, string, string, string);
+    void set(int, string, string, Date, Address);
     int getId();
     string getFirstname();
     string getLastname();
-    string getDate();
     void show();
     static int getCount();
 };
-Person::Person() : id(0), firstname("undefined"), lastname("undefined"), date("undefined")
+Person::Person() : id(0), firstname("undefined"), lastname("undefined"), date(00, 00, 0000), address("undefined", "undefined", "undefined")
 {
-    set(id, firstname, lastname, date);
+    set(id, firstname, lastname, date, address);
     ++Person::count;
 };
-Person::Person(int id, string firstname, string lastname, string date) : id(id), firstname(firstname), lastname(lastname), date(date)
+Person::Person(int id, string firstname, string lastname, Date date, Address address) : id(id), firstname(firstname), lastname(lastname), date(date), address(address)
 {
-    set(id, firstname, lastname, date);
+    set(id, firstname, lastname, date, address);
     ++Person::count;
 };
-Person::~Person() { cout << "destructuring Person : " << firstname << endl; }
-void Person::set(int id, string firstname, string lastname, string date)
+Person::~Person() { cout << "Destructuring Person : " << getId() << " : " << getFirstname() << " : " << getLastname() << endl; }
+void Person::set(int id, string firstname, string lastname, Date date, Address address)
 {
     this->id = id;
     this->firstname = firstname;
     this->lastname = lastname;
     this->date = date;
+    this->address = address;
 }
 int Person::getId() { return id; }
 string Person::getFirstname() { return firstname; }
 string Person::getLastname() { return lastname; }
-string Person::getDate() { return date; }
 void Person::show()
 {
     cout << "ID : " << id << endl;
     cout << "Firstname : " << firstname << endl;
     cout << "Lastname : " << lastname << endl;
-    cout << "Date : " << date << endl;
+    date.show();
+    address.show();
 }
 int Person::count = 0;
 int Person::getCount() { return count; }
+
+void IPerson(int &personId, string &firstname, string &lastname, Date &date, Address &address)
+{
+    cout << "Enter ID : ";
+    cin >> personId;
+    cout << "Enter Firstname : ";
+    cin >> firstname;
+    cout << "Enter Lastname : ";
+    cin >> lastname;
+    cout << "\nDate of Birth: ";
+    date.IDate();
+    cout << "\nAddress: ";
+    address.IAddress();
+}
+void OPerson(int &personId, string &firstname, string &lastname, Date &date, Address &address)
+{
+    cout << "ID : " << personId << endl;
+    cout << "Firstname : " << firstname << endl;
+    cout << "Lastname : " << lastname << endl;
+    date.show();
+    address.show();
+}
 
 #endif // PERSON_H

31ad5b419dc0349d97c6284b25042c1c3924e2fb
TaiChi <anothai.0978452316@gmail.com>
2025-05-19 01:33:47 +0700

    rfc: impl change fn

--- Books/chapter2/date.cpp
+++ Books/chapter2/date.cpp
@@ -6,8 +6,8 @@ using namespace std;
 int main()
 {
     int day, month, year;
-    IODate(day, month, year);
     Date d1, d2(day, month, year);
+    d2.IDate();
     d1.show();
     cout << endl;
     d2.show();
